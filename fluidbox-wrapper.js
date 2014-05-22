ba4f9986d14ef9500d20cd67db60f7a10d44b2b6
Duncan Johnson <duncan@duncanandmeg.org>
2014-05-22 10:04:18 -0600

    cleaned up functions.php for easier deployment

--- fluidbox-wrapper.js
+++
@@ -1,5 +0,0 @@
-jQuery(document).ready(function($) { //necessary because the fluidbox plugin responds to the '$' selector, but wordpress loads jquery in NoConflict mode[1]
-    $('#content a, a[rel="lightbox"]').fluidbox(); //triggers fluidbox on anchors within the content div
-});
-
-// [1]: http://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
--- functions.php
+++ functions.php
@@ -3,26 +3,21 @@
  
     function my_scripts_method() {
 
-        //register scripts
-        wp_register_script ( //RefTagger
-            "RefTagger",
-            get_stylesheet_directory_uri() . "/RefTagger.js",
-            array(), false, true);
-
         //Fluidbox
         wp_register_script( //Fluidbox plugin
         	"fluidbox",
         	get_stylesheet_directory_uri() . "/fluidbox/jquery.fluidbox.min.js", 
         	array( 'jquery' ), false, true); 
-        wp_register_script( //my wrapper script for Fluidbox
-            "fluidbox-wrapper",
-            get_stylesheet_directory_uri() . "/fluidbox-wrapper.js",
+
+        //wrapper script with RefTagger and fluidbox integration        
+        wp_register_script(
+            "my-wrapper",
+            get_stylesheet_directory_uri() . "/wrapper.js",
             array('fluidbox'), false, true);
 
         //enqueue scripts
         wp_enqueue_script("fluidbox"); //add Fluidbox
-        wp_enqueue_script("fluidbox-wrapper"); //add fluidbox wrapper
-        wp_enqueue_script("RefTagger");
+        wp_enqueue_script("my-wrapper"); //my wrapper scripts
 
     }
 ?>
