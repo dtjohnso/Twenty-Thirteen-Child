cb6d54905249e360624e589d1063f6a101c81f8a
Duncan Johnson <duncan@duncanandmeg.org>
2015-09-16 16:56:01 -0600

    Restrict selector from including anchor tags for fluidbox

--- wrapper.js
+++ wrapper.js
@@ -1,8 +1,10 @@
 //Fluidbox wrapper
 
 jQuery(document).ready(function($) { //necessary because the fluidbox plugin responds to the '$' selector, but wordpress loads jquery in NoConflict mode[1]
-    $('#content a, a[rel="lightbox"]').fluidbox(); //triggers fluidbox on anchors within the content div
+    $('#content a:not([href^="#"]), a[rel="lightbox"]:not([href^="#"])').fluidbox(); //triggers fluidbox on anchors within the content div
 });
+
+// See https://github.com/terrymun/Fluidbox/issues/137 for issue with # tags and fluidbox
 
 // [1]: http://codex.wordpress.org/Function_Reference/wp_enqueue_script#jQuery_noConflict_Wrappers
 
